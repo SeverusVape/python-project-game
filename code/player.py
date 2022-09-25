043d4165448c8c3c66e3e17d452abe6920a9337e
Severus Vape <koshelevworkmail@gmail.com>
2022-09-25 17:49:58 -0400

    Player moving animation and idle

--- code/player.py
+++ code/player.py
@@ -31,22 +31,39 @@ class Player(pygame.sprite.Sprite):
             full_path = "./graphics/character/" + animation
             self.animations[animation] = import_folder(full_path)
 
+    def animate(self, dt):
+        self.frame_index += 4 * dt
+        if self.frame_index >= len(self.animations[self.status]):
+            self.frame_index = 0
+
+        self.image = self.animations[self.status][int(self.frame_index)]
+
     def input(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_w]:
             self.direction.y = -1
+            self.status = "up"
         elif keys[pygame.K_s]:
             self.direction.y = 1
+            self.status = "down"
         else:
             self.direction.y = 0
 
         if keys[pygame.K_a]:
             self.direction.x = -1
+            self.status = "left"
         elif keys[pygame.K_d]:
             self.direction.x = 1
+            self.status = "right"
         else:
             self.direction.x = 0
+
+    def get_status(self):
+        # for if the player not moving
+        if self.direction.magnitude() == 0:
+            self.status = self.status.split("_")[0] + "_idle"
+        # tool use
 
     def move(self, dt):
         # normalizing the vector (all directions vector = 1, !but not less than)
@@ -62,4 +79,6 @@ class Player(pygame.sprite.Sprite):
 
     def update(self, dt):
         self.input()
+        self.get_status()
         self.move(dt)
+        self.animate(dt)
