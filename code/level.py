315444bcc57ace48f5d93f3e942481e52bd87b78
Severus Vape <koshelevworkmail@gmail.com>
2022-10-02 18:51:02 -0400

    Adding anchor to soil sprites

--- code/level.py
+++ code/level.py
@@ -6,6 +6,7 @@ from sprites import Generic, Water, Flowers, Trees, Interaction
 from transition import Transition
 from pytmx.util_pygame import load_pygame
 from support import *
+from soil import SoilLayer
 
 
 class Level:
@@ -18,6 +19,7 @@ class Level:
 		self.tree_sprites = pygame.sprite.Group()
 		self.interaction_sprites = pygame.sprite.Group()
 		#
+		self.soil_layer = SoilLayer(self.all_sprites)
 		self.setup()
 		self.overlay = Overlay(self.player)
 		self.transition = Transition(self.reset, self.player)
@@ -68,7 +70,8 @@ class Level:
 					group=self.all_sprites,
 					collision_sprites=self.collision_sprites,
 					tree_sprites=self.tree_sprites,
-					interaction=self.interaction_sprites)
+					interaction=self.interaction_sprites,
+					soil_layer=self.soil_layer)
 
 			if obj.name == "Bed":
 				Interaction(
