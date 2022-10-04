1808e4fc2b611855dc32857ebcb74af001a51d76
Severus Vape <koshelevworkmail@gmail.com>
2022-10-04 17:40:54 -0400

    Watering soil and rain logic

--- code/level.py
+++ code/level.py
@@ -7,6 +7,8 @@ from transition import Transition
 from pytmx.util_pygame import load_pygame
 from support import *
 from soil import SoilLayer
+from sky import Rain
+from random import randint
 
 
 class Level:
@@ -23,6 +25,10 @@ class Level:
 		self.setup()
 		self.overlay = Overlay(self.player)
 		self.transition = Transition(self.reset, self.player)
+		# sky
+		self.rain = Rain(self.all_sprites)
+		self.raining = randint(0, 10) > 3
+		self.soil_layer.raining = self.raining
 
 	def setup(self):
 		tmx_data = load_pygame("./data/map.tmx")
@@ -92,6 +98,10 @@ class Level:
 	def reset(self):
 		# soil
 		self.soil_layer.remove_water()
+		self.raining = randint(0, 10) > 3
+		self.soil_layer.raining = self.raining
+		if self.raining:
+			self.soil_layer.water_all()
 		# apples on trees
 		for tree in self.tree_sprites.sprites():
 			for apple in tree.apple_sprites.sprites():
@@ -104,7 +114,10 @@ class Level:
 		self.all_sprites.update(dt)
 
 		self.overlay.display()
-
+		# rain
+		if self.raining:
+			self.rain.update()
+		# transition overlay
 		if self.player.sleep:
 			self.transition.play()
 
--- code/soil.py
+++ code/soil.py
@@ -61,6 +61,8 @@ class SoilLayer:
 				if 'F' in self.grid[y][x]:
 					self.grid[y][x].append('X')
 					self.create_soil_tiles()
+					if self.raining:
+						self.water_all()
 
 	def water(self, target_pos):
 		for soil_sprite in self.soil_sprites.sprites():
@@ -70,7 +72,16 @@ class SoilLayer:
 				self.grid[y][x].append('W')
 				pos = soil_sprite.rect.topleft
 				surf = choice(self.water_surfs)
-				WaterTile(pos, surf, tuple([self.all_sprites, self.water_sprites]))
+				WaterTile(pos, surf, [self.all_sprites, self.water_sprites])
+
+	def water_all(self):
+		for index_row, row in enumerate(self.grid):
+			for index_col, cell in enumerate(row):
+				if 'X' in cell and 'W' not in cell:
+					cell.append('W')
+					x = index_col * TILE_SIZE
+					y = index_row * TILE_SIZE
+					WaterTile((x, y), choice(self.water_surfs), [self.all_sprites, self.water_sprites])
 
 	def remove_water(self):
 		for sprite in self.water_sprites.sprites():
