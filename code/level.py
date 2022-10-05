04048ee5db424ff975fce4e2e038cf30ccf8754d
Severus Vape <koshelevworkmail@gmail.com>
2022-10-05 19:46:59 -0400

    Harvesting

--- code/level.py
+++ code/level.py
@@ -2,7 +2,7 @@ import pygame
 from settings import *
 from player import Player
 from overlay import Overlay
-from sprites import Generic, Water, Flowers, Trees, Interaction
+from sprites import Generic, Water, Flowers, Trees, Interaction, Particle
 from transition import Transition
 from pytmx.util_pygame import load_pygame
 from support import *
@@ -110,10 +110,20 @@ class Level:
 				apple.kill()
 			tree.create_fruit()
 
+	def plant_collision(self):
+		if self.soil_layer.plant_sprites:
+			for plant in self.soil_layer.plant_sprites.sprites():
+				if plant.harvestable and plant.rect.colliderect(self.player.hitbox):
+					self.player_add(plant.plant_type)
+					plant.kill()
+					Particle(plant.rect.topleft, plant.image, [self.all_sprites], LAYERS['main'])
+					self.soil_layer.grid[plant.rect.centery // TILE_SIZE][plant.rect.centerx // TILE_SIZE].remove('P')
+
 	def run(self, dt):
 		self.display_surface.fill('black')
 		self.all_sprites.custom_draw(self.player)
 		self.all_sprites.update(dt)
+		self.plant_collision()
 
 		self.overlay.display()
 		# rain
